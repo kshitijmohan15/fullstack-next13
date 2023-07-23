d4b60ac6eb03bd6afef38cc2782369a1b4d0f887
kshitijmohan15 <92985242+kshitijmohan15@users.noreply.github.com>
2023-07-23 17:54:14 +0530

    initial commit

---
+++ src/db/schema.ts
@@ -0,0 +1,21 @@
+import { relations, type InferModel } from "drizzle-orm";
+import {
+	boolean,
+	decimal,
+	int,
+	json,
+	mysqlEnum,
+	mysqlTable,
+	serial,
+	text,
+	timestamp,
+	varchar,
+} from "drizzle-orm/mysql-core";
+
+export const products = mysqlTable("products", {
+	id: serial("id").primaryKey(),
+	name: varchar("userId", { length: 191 }).notNull(),
+	createdAt: timestamp("createdAt").defaultNow(),
+});
+
+export type Product = InferModel<typeof products>;
